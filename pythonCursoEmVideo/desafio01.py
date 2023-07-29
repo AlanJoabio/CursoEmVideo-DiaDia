e79cbbe6ecfd07a496528ec35fbf5fc43eba771f
LanJob <alanjmsq@gmail.com>
2023-07-29 12:50:59 -0300

    Add desafios

---
+++ pythonCursoEmVideo/desafio01.py
@@ -0,0 +1,16 @@
+num1 = int(input('Digite um valor: '));
+num2 = int(input('Digite outro valor: '));
+
+soma = num1 + num2;
+subtrair = num1 - num2;
+dividir = num1 / num2;
+
+print(soma);
+print(subtrair);
+print(dividir);
+print(f'A soma entre {num1} e {num2} vale {soma}.');
+print()
+print(f'A Substraição {num1} e {num2} vale {subtrair}');
+print()
+print(f'A Divisão {num1} e {num2} vale {dividir}');
+print()
